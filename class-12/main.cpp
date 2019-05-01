b203cdfa90391d3af59c706d79f8cc8fec3d065b
Helder Costa <heldercostaaa@gmail.com>
2019-04-30 22:24:53 -0300

    adding main

---
+++ class-12/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  class-11
+//
+//  Created by Helder Costa (github: heldercostaa) on 30/04/19.
+//  Copyright © 2019. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main() {
+	
+	return 0;
+}
